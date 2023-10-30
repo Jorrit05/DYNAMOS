3209c4cfc95b5b22303d4c8a2b3fdd42842a7c4f
Jorrit05 <jorstut@hotmail.com>
2023-10-30 09:30:41 +0100

    test-PVC

--- configuration/fill-pvc.sh
+++ configuration/fill-pvc.sh
@@ -5,11 +5,11 @@ kubectl apply -f temp-pod.yaml
 
 # Wait for the pod to be in the 'Running' state
 echo "Waiting for temp-pod to be Running..."
-kubectl wait --for=condition=Ready pod/temp-pod --timeout=300s
+kubectl wait --for=condition=Ready pod/temp-pod --timeout=300s -n core
 
 # Copy local files to the PVC
-kubectl cp ./k8s_service_files/definitions.json temp-pod:/mnt/
-kubectl cp ./k8s_service_files/rabbitmq.conf temp-pod:/mnt/
+kubectl cp ./k8s_service_files/definitions.json temp-pod:/mnt/ -n core
+kubectl cp ./k8s_service_files/rabbitmq.conf temp-pod:/mnt/ -n core
 
 # Delete the temporary pod
-kubectl delete -f temp-pod.yaml
+kubectl delete -f temp-pod.yaml -n core
