57c84634b4816f060d95b8f72374326e63a2a0f9
Jorrit05 <jorstut@hotmail.com>
2023-10-31 17:32:43 +0100

    change pvc load

--- configuration/fill-rabbit-pvc.sh
+++ configuration/fill-rabbit-pvc.sh
@@ -6,10 +6,20 @@ kubectl apply -f temp-pod.yaml
 # Wait for the pod to be in the 'Running' state
 echo "Waiting for temp-pod to be Running..."
 kubectl wait --for=condition=Ready pod/temp-pod --timeout=300s -n core
+kubectl wait --for=condition=Ready pod/temp-pod --timeout=300s -n orchestrator
 
 # Copy local files to the PVC
 kubectl cp ./k8s_service_files/definitions.json temp-pod:/mnt/ -n core
 kubectl cp ./k8s_service_files/rabbitmq.conf temp-pod:/mnt/ -n core
 
+# Create a tarball of the files
+tar -czvf etcd_files.tar.gz -C ./etcd_launch_files/ .
+
+# Copy the tarball to the pod
+kubectl cp etcd_files.tar.gz temp-pod:/mnt -n orchestrator
+
+# Untar the files inside the pod (optional, if you want to unpack the files inside the pod)
+kubectl exec -n orchestrator temp-pod -- tar -xzvf /mnt/etcd_files.tar.gz -C /mnt
+
 # Delete the temporary pod
-kubectl delete -f temp-pod.yaml -n core
+kubectl delete -f temp-pod.yaml
