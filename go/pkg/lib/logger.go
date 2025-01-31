0802829c1127dde3fcde0f06137bcc37f7fc405e
Koufakis Alexandros <alkouf@hotmail.com>
2025-01-31 13:25:29 +0100

    Update logger with human readable time
    - updated the logger pkg to print the timestamp in human readable form (ISO8601)
    - in order to update the behavior the docker containers must be rebuild
    - e.g. "make orchestrator"
    - make sure the correct tag of the container image is being deployed (e.g. in the orchestrator.yaml file)

--- go/pkg/lib/logger.go
+++ go/pkg/lib/logger.go
@@ -15,6 +15,7 @@ func InitLogger(logLevel zapcore.Level) *zap.Logger {
 	config.Level.SetLevel(logLevel)
 	config.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
 	config.EncoderConfig.EncodeCaller = zapcore.FullCallerEncoder
+	config.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder // Use ISO8601 format for human-readable time
 	config.OutputPaths = []string{"stdout"}
 	config.ErrorOutputPaths = []string{"stderr"}
 	config.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
